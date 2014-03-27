455aa418a242745a7b88d8f36a92771fdc69f167
shellfly <shell0fly@gmail.com>
2014-03-28 00:31:43 +0800

    Site updated at 2014-03-27 16:31:43 UTC

---
+++ pagedown-with-emotions/i18n/Markdown.i18n.zh_cn.js
@@ -0,0 +1,34 @@
+;(function($){
+    $.fn.markdown_editor.translations['zh-CN'] = {
+        tooltips: {
+            bold: '粗体 - Ctrl+B',
+            italic: '斜体 - Ctrl+I',
+            link: '链接 - Ctrl+L',
+            quote: '引用 - Ctrl+Q',
+            code: '代码示例 - Ctrl+K',
+            image: '图片 - Ctrl+G',
+            olist: '数字列表 - Ctrl+O',
+            ulist: '符号列表 - Ctrl+U',
+            heading: '标题 - Ctrl+H',
+            hr: '水平分割线 - Ctrl+R',
+            undo: '撤销 - Ctrl+Z',
+            redo_pc: '重做 - Ctrl+Y',
+            redo_mac: '重做 - Ctrl+Shift+Z' ,
+            emotions: '表情 - Ctrl+E'
+        },
+        dialog_text: {
+            link_title: "插入链接",
+            link_example: "<p>http://example.com/ \"标题(选填)\"</p>",
+            link_description: "请在这里填写链接描述",
+            image_title: "插入图片",
+            image_example: "<p>http://example.com/images/diagram.jpg \"标题(选填)\"</p>",
+            image_description: "请在这里填写图片描述"
+        },
+        button_text: {
+            ok: "确定",
+            cancel: "取消"
+        }
+
+        
+    }
+}(jQuery));
